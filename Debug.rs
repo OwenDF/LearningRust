53d6299251a76db4f4cc19297721af29262bb490
owen <owendalyfurlong@gmail.com>
2017-02-12 21:57:07 +0000

    Another exercise

--- Debug.rs
+++ Debug.rs
@@ -14,6 +14,32 @@ impl fmt::Display for Structure
 #[derive(Debug)]
 struct Deep(Structure);
 
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D
+{
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
 fn main()
 {
     println!("{:?} months in a year", 12);
@@ -23,4 +49,21 @@ fn main()
              actor = "actor's");
     println!("Now {} will print!", Structure(3));
     println!("Now {:?} will print!", Deep(Structure(7)));
+
+    let minmax = MinMax(0, 14);
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+    
+    println!("The big range is {big}, and the small range is {small}",
+             big = big_range,
+             small = small_range);
+
+    let point = Point2D {x: 3.7, y: 1.2};
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
 }
