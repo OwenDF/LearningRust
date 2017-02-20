039151faadd617476e42fbd87f07e8469b696b57
owen <owendalyfurlong@gmail.com>
2017-02-20 23:21:10 +0000

    Learned about Primitives and Tuples

---
+++ Primitives.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+    println!("1 + 2 = {}", 1 + 2);
+    println!("1 - 2 = {}", 1 - 2);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("One million is written as {}", 1_000_000u32);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+}
